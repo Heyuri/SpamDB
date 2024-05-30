5f1e3440e97e31a3260e6d24e4704ea472a475ce
kaguy4 <kaguya@kolyma.net>
2024-05-30 16:34:14 +0300

    Add files via upload

---
+++ module/vichan_spamdb/info.php
@@ -0,0 +1,53 @@
+<?php
+    $theme = Array();
+    
+    // Theme name
+    $theme['name'] = 'KolymaNET SpamDB Client';
+    // Description (you can use Tinyboard markup here)
+    $theme['description'] = 'Uses <a href="//example.com/spam.php">KolymaNET Anti-Spam Database</a> to automatically filter and block posts. An API key is required.';
+    $theme['version'] = 'v0.1.4';
+    
+    // Theme configuration    
+    $theme['config'] = Array();
+    
+    $theme['config'][] = Array(
+        'title' => 'API Key',
+        'name' => 'apikey',
+        'type' => 'text'
+    );
+    
+    $theme['config'][] = Array(
+        'title' => 'Verified Only',
+        'name' => 'verifonly',
+        'type' => 'checkbox',
+        'default' => true,
+        'comment' => '(verified anti-spam entries only)'
+    );
+    
+    $theme['config'][] = Array(
+        'title' => 'Timeout',
+        'name' => 'curl_timeout',
+        'type' => 'text',
+        'comment' => '(in seconds)',
+        'default' => '5',
+        'size' => '1'
+    );
+    
+    // Unique function name for building everything
+    $theme['build_function'] = 'spamdb_build';
+    $theme['install_callback'] = 'spamdb_install';
+
+    if (!function_exists('spamdb_install')) {
+        function spamdb_install($settings) {
+			
+			// Validate Timeout
+            if (!is_numeric($settings['curl_timeout']) || $settings['curl_timeout'] < 0)
+                return Array(false, '<strong>' . utf8tohtml($settings['curl_timeout']) . '</strong> is not a non-negative integer.');
+			
+			// cURL installed?
+			if (!function_exists('curl_version')){
+				return Array(false, '<strong>cURL</strong> is not installed.');
+			}
+			
+        }
+    }
