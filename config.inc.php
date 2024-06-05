3c2d2ca43a3d3cc23789428638b5324ca520650e
komeo <63191092+k0me0@users.noreply.github.com>
2024-06-05 17:42:02 +0200

    Add files via upload

--- config.inc.php
+++ config.inc.php
@@ -1,38 +1,43 @@
 <?php
-define('CAPTCHA_PHP', '');
-define('FILE_DIR', './storage/');
-define('PAGE_DEF', 40);
-define('TOP_TXT', file_get_contents('top.txt'));
 
-define('SQLHOST', 'localhost');
-define('SQLUSER', 'username');
-define('SQLPASS', 'password');
-define('SQLDB', 'spamdb');
-define('SQLTABLE_SPAM', 'spam');
+// SpamDB config file
 
-define('ALLOW_USER', true);
-define('REMOTE_ADDR', $_SERVER['REMOTE_ADDR']);
+//define('CAPTCHA_PHP', 'captcha.php');
+define('FILE_DIR', './storage/'); // Directory to store image files in
+define('PAGE_DEF', 40); // Number of displayed entries per page
+define('TOP_TXT', file_get_contents('top.txt')); // extra HTML to display in spam.php
 
+define('SQLHOST', 'localhost'); // SQL host
+define('SQLUSER', 'CHANGEME'); // SQL login
+define('SQLPASS', 'CHANGEME'); // SQL password
+define('SQLDB', 'CHANGEME'); // SQL database name
+define('SQLTABLE_SPAM', 'spam'); // SQL table name
+
+define('ALLOW_USER', true); // Allow users to submit spam
+define('REMOTE_ADDR', $_SERVER['REMOTE_ADDR']); // Don't touch
+
+// Valid API keys
 $user_keys = array(
-	'define',
+	'put',
 	'the',
 	'keys',
 	'here'
 );
 
+// Avaialable spam submission types
 $cans_of_spam = array(
-	'ADV_TXT',
-	'ADV_IMG',
-	'ADV_IP',
-	'CP_IP',
-	'PROXY_IP',
-	'MISC_IP'
+	'ADV_TXT', // Text
+	'ADV_IMG', // Image
+	'ADV_IP', // IP
+	'CP_IP', // "automated" CP spam IP
+	'PROXY_IP', // Proxy IP
+	'MISC_IP' // ?
 );
 
+// Admin passwords
 $ADMIN = array(
-	'login',
-	'passwords',
-	'go_here'
+	'CHANGEME1',
+	'CHANGEME2'
 );
 
 ?>
