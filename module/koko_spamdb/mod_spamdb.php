8dfc30b509d6c0247cf4efdd8bdb68fe4478f509
komeo <63191092+k0me0@users.noreply.github.com>
2024-06-05 17:46:42 +0200

    Add files via upload

--- module/koko_spamdb/mod_spamdb.php
+++ module/koko_spamdb/mod_spamdb.php
@@ -2,13 +2,13 @@
 require_once(ROOTPATH.'module/spamdb/spamdb_client.php');
 
 class mod_spamdb implements IModule {
-	private $API_KEY = 'apipassword';
+	private $API_KEY = 'CHANGEME';
 
 	public function __construct($PMS) {
 	}
 
 	public function getModuleName() {
-		return __CLASS__.' : K! SpamDB';
+		return __CLASS__.' : Koko SpamDB';
 	}
 
 	public function getModuleVersionInfo() {
@@ -16,14 +16,13 @@ class mod_spamdb implements IModule {
 	}
 
 	public function autoHookRegistBeforeCommit(&$name, &$email, &$sub, &$com, &$category, &$age, $dest, $isReply, $imgWH, &$status) {
-		if (defined('VIPDEF')) return;
 		$filehash = '';
 		if ($dest) {
 		    if (is_file($dest))
 		        $filehash = md5_file($dest);
 		}
 		if (spamcheck($this->API_KEY, getREMOTE_ADDR(), $com, $filehash)) {
-			error('Your post could not be submitted due to the contents of your post or your IP address being listed in our SpamDB. Contact administration if you think this is a mistake.');
+			error('Your post could not be submitted due to the contents of your post or your IP address being listed in our SpamDB.<br>Contact administration if you think this is a mistake.');
 		}
 	}
 }
